951d706250fd1a2a32a93e7a6a107e2d51039604
RaviShekharThakur <ravissj10@gmail.com>
2024-03-07 14:43:11 +0530

    used react router to fetch news category via navbar

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,23 +1,19 @@
-'use client';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Avatar, Dropdown, Navbar } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
 export default class CustomNavbar extends Component {
   render() {
     return (
-      <Navbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
+    <Navbar fluid rounded>
+      <Navbar.Brand as={Link} to="https://flowbite-react.com">
         <img src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Chatter the news</span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Chatter with the News
+        </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-          }
-        >
+        <Dropdown arrowIcon={false} inline label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />}>
           <Dropdown.Header>
             <span className="block text-sm">Bonnie Green</span>
             <span className="block truncate text-sm font-medium">name@flowbite.com</span>
@@ -31,15 +27,32 @@ export default class CustomNavbar extends Component {
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link href="/" active>
+        <Navbar.Link as={Link} to="/" active>
           Home
         </Navbar.Link>
-        <Navbar.Link href="/">News</Navbar.Link>
-        <Navbar.Link href="/">Services</Navbar.Link>
-        <Navbar.Link href="/">About</Navbar.Link>
-        <Navbar.Link href="/">Contact</Navbar.Link>
+        <Navbar.Link as={Link} to="/business">
+          Business
+        </Navbar.Link>
+        <Navbar.Link as={Link} to="/entertainment">
+          Entertainment
+        </Navbar.Link>
+        <Navbar.Link as={Link} to="/general">
+          General
+        </Navbar.Link>
+        <Navbar.Link as={Link} to="/health">
+          Health
+        </Navbar.Link>
+        <Navbar.Link as={Link} to="/science">
+          Science
+        </Navbar.Link>
+        <Navbar.Link as={Link} to="/sports">
+          Sports
+        </Navbar.Link>
+        <Navbar.Link as={Link} to="/technology">
+          Technology
+        </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-    )
+    );
   }
 }
--- src/components/News.js
+++ src/components/News.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import { Button } from 'flowbite-react';
 import  Spinner  from './Spinner';
+import PropTypes from 'prop-types'
+
 export default class News extends Component {
+
+  static defaultProps = {
+    country: 'in',
+    pageSize: 6,
+    category: 'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
 
   constructor(){
     super()
@@ -13,7 +27,7 @@ export default class News extends Component {
     }
   }
   async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a9ea2413ccbe44ef80c96819ab1857d3&page=1&pageSize=${this.props.pageSize}`
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a9ea2413ccbe44ef80c96819ab1857d3&page=1&pageSize=${this.props.pageSize}`
     this.setState({loading: true})
     let data = await fetch(url);
     let parsedData = await data.json()
@@ -23,7 +37,7 @@ export default class News extends Component {
       loading: false})
   }
   handlePrevClick = async() =>{
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a9ea2413ccbe44ef80c96819ab1857d3&page=${this.state.page -1}&pageSize=${this.props.pageSize}`
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a9ea2413ccbe44ef80c96819ab1857d3&page=${this.state.page -1}&pageSize=${this.props.pageSize}`
     this.setState({loading: true})
     let data = await fetch(url);
     let parsedData = await data.json()
@@ -37,7 +51,7 @@ export default class News extends Component {
 
   handleNextClick = async() =>{
     if (!(this.state.page + 1> Math.ceil(this.state.totalResults/this.props.pageSize))){
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a9ea2413ccbe44ef80c96819ab1857d3&page=${this.state.page +1}&pageSize=${this.props.pageSize}`
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a9ea2413ccbe44ef80c96819ab1857d3&page=${this.state.page +1}&pageSize=${this.props.pageSize}`
       this.setState({loading: true})
       let data = await fetch(url);
       let parsedData = await data.json()
