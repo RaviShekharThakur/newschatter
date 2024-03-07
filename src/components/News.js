528ebd56483a7268af3b1404fde0abd9c33c1d9e
RaviShekharThakur <ravissj10@gmail.com>
2024-03-07 11:56:50 +0530

    spinner is now visible on loading and hidden on all loading types

--- src/components/News.js
+++ src/components/News.js
@@ -14,42 +14,47 @@ export default class News extends Component {
   }
   async componentDidMount(){
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a9ea2413ccbe44ef80c96819ab1857d3&page=1&pageSize=${this.props.pageSize}`
+    this.setState({loading: true})
     let data = await fetch(url);
     let parsedData = await data.json()
-    this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+    this.setState({
+      articles: parsedData.articles, 
+      totalResults: parsedData.totalResults, 
+      loading: false})
   }
   handlePrevClick = async() =>{
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a9ea2413ccbe44ef80c96819ab1857d3&page=${this.state.page -1}&pageSize=${this.props.pageSize}`
+    this.setState({loading: true})
     let data = await fetch(url);
     let parsedData = await data.json()
     this.setState({
       page: this.state.page -1,
-      articles: parsedData.articles
+      articles: parsedData.articles,
+      loading: false
     })
 
   }
 
   handleNextClick = async() =>{
-    if (this.state.page + 1> Math.ceil(this.state.totalResults/this.props.pageSize)){
-
-    }
-    else{
+    if (!(this.state.page + 1> Math.ceil(this.state.totalResults/this.props.pageSize))){
       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a9ea2413ccbe44ef80c96819ab1857d3&page=${this.state.page +1}&pageSize=${this.props.pageSize}`
+      this.setState({loading: true})
       let data = await fetch(url);
       let parsedData = await data.json()
       this.setState({
         page: this.state.page +1,
-        articles: parsedData.articles
+        articles: parsedData.articles,
+        loading: false
       })
     }
   }
   render() {
     return (
       <>
-        <Spinner/>
+        {this.state.loading && <Spinner/>}
         <div className="container mx-auto p-4">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {this.state.articles.map((element)=>{
+            {!this.state.loading && this.state.articles.map((element)=>{
               return <NewsItem key={element.url} title= {element.title} description= {element.description} imageurl= {element.urlToImage} newsurl= {element.url}/>
             })}
           </div>
