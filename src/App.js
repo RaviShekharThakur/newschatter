a5e98a446d13fd235a5ffea23b9387f6fee62640
RaviShekharThakur <ravissj10@gmail.com>
2024-03-08 16:01:05 +0530

    sticky navbar and api key used from Env

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default class App extends Component {
   pageSize = 6
+  apiKey= process.env.REACT_APP_CHATTER_NEWS_API
   state = {
     progress: 0
   }
@@ -21,18 +22,18 @@ export default class App extends Component {
           <CustomNavbar />
           <LoadingBar
             color='#f11946'
-            height={3}
+            height={3}  
             progress={this.state.progress}
           />
           <Routes>
-            <Route exact path="/business" element={<News key='business' setProgress={this.setProgress} pageSize={this.pageSize} country="in" category="business" />} />
-            <Route exact path="/" element={<News key='general' setProgress={this.setProgress} pageSize={this.pageSize} country="in" category="general" />} />
-            <Route exact path="/entertainment" element={<News key='entertainment' setProgress={this.setProgress} pageSize={this.pageSize} country="in" category="entertainment" />} />
-            <Route exact path="/general" element={<News key='general' setProgress={this.setProgress} pageSize={this.pageSize} country="in" category="general" />} />
-            <Route exact path="/health" element={<News key='health' setProgress={this.setProgress} pageSize={this.pageSize} country="in" category="health" />} />
-            <Route exact path="/science" element={<News key='science' setProgress={this.setProgress} pageSize={this.pageSize} country="in" category="science" />} />
-            <Route exact path="/sports" element={<News key='sports' setProgress={this.setProgress} pageSize={this.pageSize} country="in" category="sports" />} />
-            <Route exact path="/technology" element={<News key='technology' setProgress={this.setProgress} pageSize={this.pageSize} country="in" category="technology" />} />
+            <Route exact path="/business" element={<News key='business' setProgress={this.setProgress} apiKey={this.apiKey} pageSize={this.pageSize} country="in" category="business" />} />
+            <Route exact path="/" element={<News key='general' setProgress={this.setProgress} apiKey={this.apiKey} pageSize={this.pageSize} country="in" category="general" />} />
+            <Route exact path="/entertainment" element={<News key='entertainment' setProgress={this.setProgress} apiKey={this.apiKey} pageSize={this.pageSize} country="in" category="entertainment" />} />
+            <Route exact path="/general" element={<News key='general' setProgress={this.setProgress} apiKey={this.apiKey} pageSize={this.pageSize} country="in" category="general" />} />
+            <Route exact path="/health" element={<News key='health' setProgress={this.setProgress} apiKey={this.apiKey} pageSize={this.pageSize} country="in" category="health" />} />
+            <Route exact path="/science" element={<News key='science' setProgress={this.setProgress} apiKey={this.apiKey} pageSize={this.pageSize} country="in" category="science" />} />
+            <Route exact path="/sports" element={<News key='sports' setProgress={this.setProgress} apiKey={this.apiKey} pageSize={this.pageSize} country="in" category="sports" />} />
+            <Route exact path="/technology" element={<News key='technology' setProgress={this.setProgress} apiKey={this.apiKey} pageSize={this.pageSize} country="in" category="technology" />} />
           </Routes>
         </Router>
       </div>
--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -5,54 +5,56 @@ import { Link } from 'react-router-dom';
 export default class CustomNavbar extends Component {
   render() {
     return (
-    <Navbar fluid rounded>
-      <Navbar.Brand as={Link} to="https://flowbite-react.com">
-        <img src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Chatter with the News
-        </span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Dropdown arrowIcon={false} inline label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />}>
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
-        <Navbar.Toggle />
-      </div>
-      <Navbar.Collapse>
-        <Navbar.Link as={Link} to="/" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link as={Link} to="/business">
-          Business
-        </Navbar.Link>
-        <Navbar.Link as={Link} to="/entertainment">
-          Entertainment
-        </Navbar.Link>
-        <Navbar.Link as={Link} to="/general">
-          General
-        </Navbar.Link>
-        <Navbar.Link as={Link} to="/health">
-          Health
-        </Navbar.Link>
-        <Navbar.Link as={Link} to="/science">
-          Science
-        </Navbar.Link>
-        <Navbar.Link as={Link} to="/sports">
-          Sports
-        </Navbar.Link>
-        <Navbar.Link as={Link} to="/technology">
-          Technology
-        </Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
+    <div  style={{ position: 'sticky', top: '0', zIndex: '1000' }}>
+      <Navbar fluid rounded>
+        <Navbar.Brand as={Link} to="https://flowbite-react.com">
+          <img src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Chatter with the News
+          </span>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Dropdown arrowIcon={false} inline label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />}>
+            <Dropdown.Header>
+              <span className="block text-sm">Bonnie Green</span>
+              <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+            </Dropdown.Header>
+            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Item>Settings</Dropdown.Item>
+            <Dropdown.Item>Earnings</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link as={Link} to="/" active>
+            Home
+          </Navbar.Link>
+          <Navbar.Link as={Link} to="/business">
+            Business
+          </Navbar.Link>
+          <Navbar.Link as={Link} to="/entertainment">
+            Entertainment
+          </Navbar.Link>
+          <Navbar.Link as={Link} to="/general">
+            General
+          </Navbar.Link>
+          <Navbar.Link as={Link} to="/health">
+            Health
+          </Navbar.Link>
+          <Navbar.Link as={Link} to="/science">
+            Science
+          </Navbar.Link>
+          <Navbar.Link as={Link} to="/sports">
+            Sports
+          </Navbar.Link>
+          <Navbar.Link as={Link} to="/technology">
+            Technology
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
     );
   }
 }
